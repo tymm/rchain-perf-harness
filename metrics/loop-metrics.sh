0041c4065307f4e978ff740be0c18b868e05576f
Dominik Zajkowski <dzajkowski@invisibl.es>
2018-08-13 16:22:53 +0200

    better 'or true'

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -2,9 +2,9 @@
 set -axe
 
 while true; do 
-    curl http://bootstrap:39992 || true > bootstrap.txt
+    (curl http://bootstrap:39992 > bootstrap.txt) || true
     cat bootstrap.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/bootstrap
-    curl http://validator1:30012 || true > validator1.txt
+    (curl http://validator1:30012 > validator1.txt) || true
     cat validator1.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/validator1
     sleep 10
 done
