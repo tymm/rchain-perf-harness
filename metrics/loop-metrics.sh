d55f890ea1bba503912624bcf398d54bc9b1e0fa
Dominik Zajkowski <dzajkowski@invisibl.es>
2018-08-13 16:13:10 +0200

    remove 'never fail' on pushing to prometheus gateway

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -3,8 +3,8 @@ set -axe
 
 while true; do 
     curl http://bootstrap:39992 || true > bootstrap.txt
-    cat bootstrap.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/bootstrap || true
+    cat bootstrap.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/bootstrap
     curl http://validator1:30012 || true > validator1.txt
-    cat validator1.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/validator1 || true
+    cat validator1.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/validator1
     sleep 10
 done
