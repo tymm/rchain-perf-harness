1ece75e7e1c60339c857b343ad6d52ae7189aac4
Dominik Zajkowski <dzajkowski@invisibl.es>
2018-08-14 22:02:52 +0200

    setup more validation nodes

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -2,9 +2,9 @@
 set -axe
 
 while [ ! -f ./stop-loop ]; do 
-    (curl http://bootstrap:39992 > bootstrap.txt) || true
-    cat bootstrap.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/bootstrap
-    (curl http://validator1:30012 > validator1.txt) || true
-    cat validator1.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/validator1
+    for instance in bootstrap validator1 validator2 validator3 validator4; do 
+        (curl http://$instance:39992 > $instance.txt) || true
+        cat $instance.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/$instance
+    done
     sleep 10
 done
