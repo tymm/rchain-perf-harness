4740b2e96c207b1581dada4cb6d9125d6dc74a65
Dominik Zajkowski <dzajkowski@invisibl.es>
2018-08-15 12:55:14 +0200

    reverted port change

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -4,7 +4,7 @@ set -axe
 while [ ! -f ./stop-loop ]; do 
     for instance in bootstrap validator1 validator2 validator3 validator4; do 
         (curl http://$instance:30012 > $instance.txt) || true
-        cat $instance.txt | curl --data-binary @- http://prometheus-pushgateway:19091/metrics/job/${DRONE_COMMIT_SHA}/instance/$instance
+        cat $instance.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/$instance
     done
     sleep 10
 done
