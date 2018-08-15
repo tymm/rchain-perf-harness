78e557b217f55bdfefdf454bfeab6664306adef7
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-08-15 13:43:41 +0200

    Push and scrape metrics every second

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -axe
 
-while [ ! -f ./stop-loop ]; do 
+while [ ! -f ./stop-loop ]; do
     (curl http://bootstrap:39992 > bootstrap.txt) || true
     cat bootstrap.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/bootstrap
     (curl http://validator1:30012 > validator1.txt) || true
     cat validator1.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/validator1
-    sleep 10
+    sleep 1
 done
