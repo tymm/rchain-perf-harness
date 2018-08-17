c03aec50aa1b858c8848fcaa0ef878dc4061fed2
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-08-17 14:35:00 +0200

    Add hack

--- drone/drone-custom-contract.sh
+++ drone/drone-custom-contract.sh
@@ -5,4 +5,4 @@ buildNumber=$1
 contract=$2
 image=$3
 
-./drone-cli.sh deploy --param RNODE_IMAGE_VERSION=${image:-dev} --param CONTRACT=/workdir/rchain-perf-harness/$contract lukasz-golebiewski-org/rchain-perf-harness $buildNumber custom_contract
+./drone-cli.sh deploy --param CONTRACT=/workdir/rchain-perf-harness/$contract --param RNODE_IMAGE_VERSION=${image:-dev} lukasz-golebiewski-org/rchain-perf-harness $buildNumber custom_contract
