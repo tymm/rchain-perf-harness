76a2cfc9ea069d0241a7c21ae8338386652e3250
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-08-19 16:54:01 +0200

    Do not display DRONE_TOKEN

--- drone/drone-cli.sh
+++ drone/drone-cli.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ -z ${DRONE_TOKEN+x} ]; then echo "Export the DRONE_TOKEN"; exit 1; else echo "DRONE_TOKEN is set to '$DRONE_TOKEN'"; fi
+if [ -z ${DRONE_TOKEN+x} ]; then echo "Export the DRONE_TOKEN"; exit 1; fi
 
 export DRONE_SERVER=http://stress-docker.pyr8.io:8080
-
 docker run --rm -e DRONE_SERVER=$DRONE_SERVER -e DRONE_TOKEN=$DRONE_TOKEN drone/cli ${@:1:99}
