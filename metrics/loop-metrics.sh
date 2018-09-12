98fa89c76bb58255b2263d2eae9e19d3943b2682
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-09-12 18:28:44 +0200

    Fix metrics endpoint

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -axe
 
-while [ ! -f ./stop-loop ]; do 
-    for instance in bootstrap validator1 validator2 validator3 validator4; do 
-        (curl http://$instance:30012 > $instance.txt) || true
+while [ ! -f ./stop-loop ]; do
+    for instance in bootstrap validator1 validator2 validator3 validator4; do
+        (curl http://$instance:30012/metrics > $instance.txt) || true
         cat $instance.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/$instance
     done
     sleep 5
