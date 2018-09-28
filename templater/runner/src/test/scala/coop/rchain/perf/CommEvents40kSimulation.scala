5646243ddb3d9b057aac84d2cf8b0f2c1c92ac87
ys-pyrofex <yaraslau@pyrofex.net>
2018-09-28 13:08:16 +0300

    Check that mvcepp is launched as expected

---
+++ templater/runner/src/test/scala/coop/rchain/perf/CommEvents40kSimulation.scala
@@ -0,0 +1,50 @@
+package coop.rchain.perf
+import java.nio.file.{Files, Paths}
+
+import collection.JavaConverters._
+import com.typesafe.config.ConfigFactory
+import io.gatling.core.Predef.{Simulation, atOnceUsers, scenario}
+import io.gatling.core.Predef._
+
+import scala.concurrent.duration._
+import scala.io.Source
+
+/*
+ We expect to reach 40k comm events per second one day
+ */
+class CommEvents40kSimulation extends Simulation {
+
+  import RNodeActionDSL._
+
+  val iterationsCount = 100
+  val path = Paths.get("simulations/mvcepp/mvcepp.rho").toRealPath()
+
+  val contract = List("")
+
+  val conf = ConfigFactory.load()
+  val rnodes = conf.getStringList("rnodes").asScala.toList
+
+  println("")
+  val rhoContent = Source.fromFile(path.toUri).mkString
+  val contracts = List((path.getFileName.toString, rhoContent))
+
+  println(
+    s"will run ${iterationsCount} iterations of minimal comm event producing program on ${rnodes
+      .mkString(", ")}")
+  println("-------------------------------")
+  println(contracts)
+  println("-------------------------------")
+
+  val protocol = RNodeProtocol.createFor(rnodes)
+
+  val scn = scenario("CommEvents40kSimulation")
+    .foreach(contracts, "contract") {
+      repeat(iterationsCount) {
+        exec(deploy())
+      }.exec(propose())
+    }
+
+  setUp(
+    scn.inject(rampUsers(1) over (5 seconds))
+  ).protocols(protocol)
+}
