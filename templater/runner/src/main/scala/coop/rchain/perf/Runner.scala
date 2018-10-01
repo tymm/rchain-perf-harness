e2900e15025c8052b3901b7d8a84733f8ece4564
ys-pyrofex <yaraslau@pyrofex.net>
2018-10-01 19:45:36 +0300

    PhloLimit, PhloPrice

--- templater/runner/src/main/scala/coop/rchain/perf/Runner.scala
+++ templater/runner/src/main/scala/coop/rchain/perf/Runner.scala
@@ -9,9 +9,10 @@ import coop.rchain.casper.protocol.DeployServiceGrpc.{
 import coop.rchain.casper.protocol.{
   DeployData,
   DeployServiceGrpc,
-  DeployServiceResponse
+  DeployServiceResponse,
+  PhloLimit,
+  PhloPrice
 }
-import coop.rchain.rholang.interpreter
 import io.gatling.commons.util.RoundRobin
 import io.gatling.commons.stats.{KO, OK}
 import io.gatling.core.CoreComponents
@@ -63,8 +64,8 @@ object Deploy {
       .withTimestamp(System.currentTimeMillis())
       .withTerm(contract)
       .withFrom("0x1")
-      .withPhloLimit(accounting.MAX_VALUE)
-      .withPhloPrice(1)
+      .withPhloLimit(PhloLimit(Long.MaxValue))
+      .withPhloPrice(PhloPrice(1))
       .withNonce(0)
     val r = client.client.doDeploy(d)
     r.map { res =>
