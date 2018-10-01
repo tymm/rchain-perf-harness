ae18235d7e94cfb1a5f6acdf9586c7d3e53dc44c
ys-pyrofex <yaraslau@pyrofex.net>
2018-10-01 19:45:36 +0300

    PhloLimit, PhloPrice

--- templater/runner/src/main/scala/coop/rchain/perf/Runner.scala
+++ templater/runner/src/main/scala/coop/rchain/perf/Runner.scala
@@ -9,7 +9,9 @@ import coop.rchain.casper.protocol.DeployServiceGrpc.{
 import coop.rchain.casper.protocol.{
   DeployData,
   DeployServiceGrpc,
-  DeployServiceResponse
+  DeployServiceResponse,
+  PhloLimit,
+  PhloPrice
 }
 import io.gatling.commons.util.RoundRobin
 import io.gatling.commons.stats.{KO, OK}
@@ -62,8 +64,8 @@ object Deploy {
       .withTimestamp(System.currentTimeMillis())
       .withTerm(contract)
       .withFrom("0x1")
-      .withPhloLimit(0)
-      .withPhloPrice(0)
+      .withPhloLimit(PhloLimit(Long.MaxValue))
+      .withPhloPrice(PhloPrice(1))
       .withNonce(0)
     val r = client.client.doDeploy(d)
     r.map { res =>
@@ -230,10 +232,7 @@ object RNodeProtocol {
                   .forAddress(host, port)
                   .usePlaintext(true)
                   .build
-                ClientWithDetails(
-                  DeployServiceGrpc.stub(channel),
-                  host,
-                  port)
+                ClientWithDetails(DeployServiceGrpc.stub(channel), host, port)
             }
           val pool = RoundRobin(clients.toIndexedSeq)
           RNodeComponents(rnodeProtocol, clients, pool)
